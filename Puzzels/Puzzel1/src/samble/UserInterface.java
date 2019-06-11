1c79c6afe37abf3a8dd640032257325e78e2e2d7
Pim Haverdil <pim.haverdil@student.graafschapcollege.nl>
2019-06-11 13:47:56 +0200

    push

---
+++ Puzzels/Puzzel1/src/samble/UserInterface.java
@@ -0,0 +1,12 @@
+package samble;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
+public class UserInterface {
+
+    public UserInterface(GridPane root)
+    {
+
+    }
+}
