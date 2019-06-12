ad3d07d213ad45d7159db03c77b6531c83201b39
Pim Haverdil <pim.haverdil@student.graafschapcollege.nl>
2019-06-12 12:45:32 +0200

    Cable class + images

---
+++ Puzzels/Puzzel1/PipePuzzel/BentCable.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BentCable here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BentCable extends Cable
+{
+    /**
+     * Act - do whatever the BentCable wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Puzzels/Puzzel1/PipePuzzel/StraightCable.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StraightCable here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StraightCable extends Cable
+{
+    /**
+     * Act - do whatever the StraightCable wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
